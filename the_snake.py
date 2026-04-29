5f0795f77079ba4f7463e15920503138a0a66585
Щербакова Наталья <sherbakovanatalia30@gmail.com>
2026-04-30 00:31:06 +0300

    add new def handle_keys(game_object)

--- the_snake.py
+++ the_snake.py
@@ -149,4 +149,21 @@ class Snake(GameObject):
         screen.fill(BOARD_BACKGROUND_COLOR)
 
 
+        def handle_keys(game_object):
+            """Обрабатывает нажатия клавиш для изменения направления движения."""
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    raise SystemExit
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP and game_object.direction != DOWN:
+                        game_object.next_direction = UP
+                    elif event.key == pygame.K_DOWN and game_object.direction != UP:
+                        game_object.next_direction = DOWN
+                    elif event.key == pygame.K_LEFT and game_object.direction != RIGHT:
+                        game_object.next_direction = LEFT
+                    elif event.key == pygame.K_RIGHT and game_object.direction != LEFT:
+                        game_object.next_direction = RIGHT
+
+
         
